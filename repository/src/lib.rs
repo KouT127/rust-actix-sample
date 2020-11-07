f9be81b35f982cb04dc82b16612971561d8a34e6
Koichi Tada <kou26haru@gmail.com>
2020-11-07 20:07:20 +0900

    modify: replace quaint

--- repository/src/lib.rs
+++ repository/src/lib.rs
@@ -1,13 +1,21 @@
 #[macro_use]
 extern crate diesel;
 
-use diesel::prelude::*;
-use diesel::r2d2::ConnectionManager;
+use chrono::Utc;
 use diesel::result::Error::RollbackTransaction;
 use diesel::{insert_into, select, update, MysqlConnection};
 use model::context::{MySqlPool, MysqlPooled, Repository};
 use model::task::Task;
 use model::user::{NewUser, User};
+use mysql_async::prelude::Queryable;
+use mysql_async::{Params, Value};
+use quaint::connector::ResultSetIterator;
+use quaint::connector::SqlFamily::Mysql;
+use quaint::prelude::Select;
+use quaint::visitor::Visitor;
+use std::collections::HashMap;
+use std::hash::BuildHasherDefault;
+use std::ops::{Add, Deref};
 
 no_arg_sql_function!(
     last_insert_id,
@@ -19,6 +27,7 @@ pub fn get_url_from_env() -> String {
 }
 
 pub fn new_pool(url: String, pool_size: u32) -> MySqlPool {
+    use diesel::r2d2::ConnectionManager;
     let manager = ConnectionManager::<MysqlConnection>::new(url);
     r2d2::Pool::builder()
         .max_size(pool_size)
@@ -32,24 +41,23 @@ pub trait TaskRepository {
 
 impl TaskRepository for Repository {
     fn find_task(conn: &MysqlPooled, task_id: u64) -> anyhow::Result<(Task, User)> {
-        use model::schema::*;
-
-        tasks::table
-            .inner_join(users::table.on(users::dsl::id.eq(tasks::dsl::id)))
-            .filter(tasks::dsl::id.eq(task_id))
-            .first::<(Task, User)>(conn)
-            .map_err(anyhow::Error::new)
+        Ok((
+            Task {
+                id: 0,
+                user_id: 0,
+                title: "".to_string(),
+                is_done: false,
+                created_at: Utc::now().naive_utc(),
+                updated_at: None,
+            },
+            User {
+                id: 0,
+                name: "".to_string(),
+                created_at: Utc::now().naive_utc(),
+                updated_at: None,
+            },
+        ))
     }
-}
-
-
-
-pub async fn new_redis_client(redis_url: &str) -> anyhow::Result<redis::aio::MultiplexedConnection> {
-    let client = redis::Client::open(redis_url).map_err(anyhow::Error::new)?;
-    client
-        .get_multiplexed_async_std_connection()
-        .await
-        .map_err(anyhow::Error::new)
 }
 
 pub trait UserRepository {
@@ -61,58 +69,113 @@ pub trait UserRepository {
 
 impl UserRepository for Repository {
     fn find_users(conn: &MysqlPooled) -> anyhow::Result<Vec<User>> {
-        use model::schema::users::dsl::{id, users};
-        users
-            .limit(10)
-            .order(id.desc())
-            .load::<User>(conn)
-            .map_err(anyhow::Error::new)
+        Ok(Vec::new())
     }
 
     fn find_user(conn: &MysqlPooled, user_id: u64) -> anyhow::Result<User> {
-        use model::schema::users::dsl::{id, users};
-        users
-            .filter(id.eq(user_id))
-            .first::<User>(conn)
-            .map_err(anyhow::Error::new)
+        Ok(User {
+            id: 0,
+            name: "".to_string(),
+            created_at: Utc::now().naive_utc(),
+            updated_at: None,
+        })
     }
 
     fn create_user(conn: &MysqlPooled, user: &NewUser) -> anyhow::Result<User> {
-        use model::schema::users::dsl::users;
-        insert_into(users)
-            .values(user)
-            .execute(conn)
-            .map_err(|error| {
-                println!("{}", error);
-                RollbackTransaction
-            })?;
-        let generated_id: u64 = select(last_insert_id).first(conn).unwrap();
-        let new_user = user.to_user(generated_id);
-        Ok(new_user)
+        Ok(User {
+            id: 0,
+            name: "".to_string(),
+            created_at: Utc::now().naive_utc(),
+            updated_at: None,
+        })
     }
 
     fn update_user(conn: &MysqlPooled, user: &User) -> anyhow::Result<User> {
-        use model::schema::users::dsl::{id, name, users};
-        update(users.filter(id.eq(user.id)))
-            .set(name.eq(user.name.to_owned()))
-            .execute(conn)
-            .map_err(|error| {
-                println!("{}", error);
-                RollbackTransaction
-            })?;
-        Ok(user.clone())
+        Ok(User {
+            id: 0,
+            name: "".to_string(),
+            created_at: Utc::now().naive_utc(),
+            updated_at: None,
+        })
     }
+}
+
+async fn build_query() -> Vec<User> {
+    use quaint::{prelude::*, single::Quaint};
+    let conn = Quaint::new("").await;
+
+    // let conditions = "word"
+    //     .equals("meow")
+    //     .and("age".less_than(10))
+    //     .and("paw".equals("warm"));
+
+    let query = Select::from_table("users").limit(10);
+    let result = conn.unwrap().select(query).await.unwrap();
+    let result_iterator: ResultSetIterator = result.into_iter();
+    result_iterator
+        .map(|row| User {
+            id: row[0].as_i64().unwrap() as u64,
+            name: row[1].as_str().unwrap().to_owned(),
+            created_at: Utc::now().naive_local(),
+            updated_at: None,
+        })
+        .collect::<Vec<User>>()
+}
+
+async fn hoge() {
+    use mysql_async::prelude::*;
+
+    let pool = mysql_async::Pool::new("");
+    let mut conn = pool.get_conn().await.unwrap();
+    let mut v = HashMap::with_hasher(BuildHasherDefault::default());
+
+    let statement = "SELECT customer_id, amount, account_name 
+        from users 
+        where id = :foo"
+        .to_owned();
+
+    let statement = statement.add("o = :ok");
+    v.insert("test".to_owned(), Value::Bytes("".to_owned().into_bytes()));
+
+    let statement = conn.prep(statement).await.unwrap();
+
+    let users = conn
+        .exec_map(
+            &statement,
+            Params::Named(v),
+            |(id, name, created_at, updated_at)| User {
+                id,
+                name,
+                created_at,
+                updated_at,
+            },
+        )
+        .await;
+    let users = conn
+        .query_map(
+            "SELECT customer_id, amount, account_name from payment",
+            |(id, name, created_at, updated_at)| User {
+                id,
+                name,
+                created_at,
+                updated_at,
+            },
+        )
+        .await;
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{UserRepository, get_url_from_env, new_pool, new_redis_client};
+    use crate::{get_url_from_env, new_pool, UserRepository};
     use chrono::{Duration, DurationRound, Utc};
     use diesel::result::Error;
     use diesel::result::Error::RollbackTransaction;
     use diesel::Connection;
     use model::context::{MysqlPooled, Repository};
     use model::user::{NewUser, User};
+    use quaint::ast::Insert;
+    use quaint::prelude::*;
+    use quaint::single::Quaint;
 
     fn new_user(conn: &MysqlPooled) -> User {
         let new = NewUser {
@@ -129,6 +192,24 @@ mod tests {
             ),
         };
         Repository::create_user(&conn, &new).unwrap()
+    }
+
+    #[tokio::test]
+    async fn build_query() {
+        tokio::spawn(async {
+            assert!(dotenv::from_filename(".env.test").is_ok());
+            let url = get_url_from_env();
+            let conn = Quaint::new(url.as_str()).await.unwrap();
+            let insert = Insert::single_into("users").value("name", "test").build();
+            match conn.insert(insert.clone()).await {
+                Err(error) => println!("{:?}", error),
+                _ => {}
+            }
+            let result = conn.insert(insert).await;
+            println!("{:?}", result);
+        })
+        .await
+        .unwrap();
     }
 
     #[test]
@@ -192,11 +273,5 @@ mod tests {
             assert_eq!(updated_user, user.unwrap());
             Err(RollbackTransaction)
         });
-    }
-
-    #[tokio::test]
-    async fn connect_redis() {
-       let client = new_redis_client("redis://localhost:6379").await;
-       assert!(client.is_ok())
     }
 }
